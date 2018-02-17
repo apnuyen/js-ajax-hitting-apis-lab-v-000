4224817e747126acb99129cafa67ab10f6e6c3e2
An Nuyen <annuyen@gmail.com>
2018-02-17 02:11:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,58 +1,54 @@
-function displayCommits() {
-  const commits = JSON.parse(this.responseText)
-  console.log(this.responseText)
-  const commitsList = `<ul>${commits.map(commit => '<li><h3>' + commit.commit.author.name + ' (' + commit.author.login + ')</h3>' commit.commit.message + '</li>').join('')}</ul>`
-
-  document.getElementById('details').innerHTML = commitsList
-}
-
 function getRepositories() {
-  const name = document.getElementById('username').value;
-  const xhr = new XMLHttpRequest();
-  xhr.addEventListener('load', displayRepositories);
-  xhr.open("GET", `https://api.github.com/users/${username}/repos`);
-  xhr.send()
+  const username = document.getElementById("username").value;
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", displayRepositories);
+  req.open('GET', `https://api.github.com/users/${username}/repos`);
+  req.send();
 }
 
 function displayRepositories() {
-  const repos = JSON.parse(this.responseText)
+  var repos = JSON.parse(this.responseText)
   console.log(this.responseText)
-  const repoList = "<ul>" + repos.map(repo => {
-    const dataUsername = 'data-username="' + repo.owner.login + '"'
-    const dataRepoName = 'data-repository="' + repo.name + '"'
-    return(`<li>
-            <h2>${repo.name}</h2>
-            <a href="${repo.html_url}">URL</a><br>
-            <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
-            <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a>
-            </li>`)}).join('') + "</ul>";
-
-  document.getElementById('repositories').innerHTML = repoList
+  const repoList = "<ul>" + repos.map(r => {
+    const dataUsername = 'data-username="' + r.owner.login + '"'
+    const dataRepoName = 'data-repository="' + r.name + '"'
+    return (`
+      <li>
+        <h2>${r.name}</h2>
+        <a href="${r.html_url}">URL</a><br>
+        <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+        <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
+      </li>
+      `)
+    }).join('') + "</ul>"
+  document.getElementById("repositories").innerHTML = repoList
 }
 
 function getCommits(el) {
-  const repoName = el.dataset.repository
-  const uri = "https://api.github.com/repos/" + el.dataset.username + "/" + repoName + "/commits"
-  const xhr = new XMLHttpRequest()
-  xhr.addEventListener('load', displayCommits)
-  xhr.open('GET', uri)
-  xhr.send()
+  const url = "https://api.github.com/repos/" + el.dataset.username + "/" + el.dataset.repository + "/commits"
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", displayCommits)
+  req.open("GET", url)
+  req.send()
 }
 
-
+function displayCommits() {
+  const commits = JSON.parse(this.responseText)
+  console.log(this.responseText)
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.author.name + ' - ' + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById("details").innerHTML = commitsList
+}
 
 function getBranches(el) {
-  const repoName = el.dataset.repository
-  const uri = "https://api.github.com/repos/" + el.dataset.username + "/" + repoName + "/branches"
-  const xhr = new XMLHttpRequest()
-  xhr.addEventListener('load', displayBranches)
-  xhr.open("GET", uri)
-  xhr.send()
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", displayBranches)
+  req.open("GET", "https://api.github.com/repos/" + el.dataset.username + "/" + el.dataset.repository + "/branches")
+  req.send()
 }
 
 function displayBranches() {
   const branches = JSON.parse(this.responseText)
+  console.log(this.responseText)
   const branchesList = `<ul>${branches.map(branch => '<li>' + branch.name + '</li>').join('')}</ul>`
-
-  document.getElementById('details').innerHTML = branchesList
+  document.getElementById("details").innerHTML = branchesList
 }
