eb3c44d74e41f8a1b88a1f9c44bcbf72d0d2b252
An Nuyen <annuyen@gmail.com>
2018-02-17 01:59:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function displayRepositories() {
   const repos = JSON.parse(this.responseText)
   const repoList = "<ul>" + repos.map(repo => {
     const dataUsername = 'data-username="' + repo.owner.login + '"'
-    const dataRepoName = 'data-repository"' + repo.name + '"'
+    const dataRepoName = 'data-repository="' + repo.name + '"'
     return(`<li>
             <h2>${repo.name}</h2>
             <a href="${repo.html_url}">${repo.html_url}</a><br>
