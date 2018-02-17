37cd77247ecec0212882eec01a91ebbd9ad62599
An Nuyen <annuyen@gmail.com>
2018-02-17 01:46:05 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+const rootURL = "https://api.github.com"
+
+function getRepositories() {
+  const name = document.getElementById('username').value
+  const uri = new XMLHttpRequest()
+  xhr.addEventListener('load', displayRepositories)
+  xhr.open("GET", uri)
+  xhr.send()
+  return false;
+}
