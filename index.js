ebed7763a487f66174c2bcd2dcc75940808dda58
An Nuyen <annuyen@gmail.com>
2018-02-17 01:52:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,33 @@ function getRepositories() {
   xhr.send()
   return false;
 }
+
+function displayRepositories() {
+  const repos = JSON.parse(this.responseText)
+  const repoList = "<ul>" + repos.map(repo => {
+    const dataUsername = 'date-username="' + repo.owner.login + '"'
+    const dataRepoName = 'data-repository"' + repo.name + '"'
+    return(`<li>
+            <h2>${repo.name}</h2>
+            <a href="${repo.html_url}">${repo.html_url}</a><br>
+            <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+            <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
+            
+            `)
+  }).join('') + "</ul>";
+
+  document.getElementById('repositories').innerHTML = repoList
+}
+
+function getCommits(el) {
+  const repoName = el.dataset.repository
+  const uri = rootURL + "/repos/" + el.dataset.username + "/" + repoName + "/commits"
+  const xhr = new XMLHttpRequest()
+  xhr.addEventListener('load', displayCommits)
+  xhr.open('GET', uri)
+  xhr.send()
+}
+
+function displayCommits() {
+  
+}
